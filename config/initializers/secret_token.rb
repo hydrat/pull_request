1a9a4c14f448bff4f51e39330d223ea2e2bc8767
Christian Rohrer <hydrat@gmail.com>
2012-12-11 16:36:05 +0100

    initial push

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PullRequest::Application.config.secret_token = 'cf1a7fbbb35b1b5ecf5977ec96c9cbde0f95fdb6c3e7d3de332b9bff46a1627d010fed3af9176845c24025bf3421b6db00f61718d70a8b6a428e1a584238145c'
